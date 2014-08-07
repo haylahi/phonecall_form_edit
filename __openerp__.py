28df7e859eb4264c357cc9b50d038daa05b396c4
Jairo Llopis <j.llopis@grupoesoc.es>
2014-08-07 13:57:47 +0200

    Hello World.

---
+++ __openerp__.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+
+# OpenERP, Open Source Management Solution
+# Copyright (C) 2014  Grupo ESOC <www.grupoesoc.es>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+{
+    "name": "Phonecall Form Edit",
+    "version": "1.0",
+    "category": "Customer Relationship Management",
+    "summary": "Edit phonecalls with form by default",
+    "description":
+        "Phone calls are created and edited in form view by default.",
+    "author": "Grupo ESOC",
+    "website": "http://www.grupoesoc.es",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends" : ["crm"],
+    "data" : [
+        "view/phonecall.xml",
+    ],
+}
